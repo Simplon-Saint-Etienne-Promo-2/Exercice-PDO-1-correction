cc7b1a9296414412769c41024a289645f69a7463
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2023-12-14 10:04:50 +0100

    exercice 4 correction

--- exercice-4/index.php
+++ exercice-4/index.php
@@ -1,4 +1,11 @@
 <?php
+require_once('./database_connect.php');
+
+$request = $database->query('SELECT * FROM clients 
+INNER JOIN cards ON clients.cardNumber = cards.cardNumber 
+WHERE cards.cardTypesId = 1');
+
+$clientsWithFidelityCard = $request->fetchAll();
 
 
 
@@ -6,12 +13,23 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <h2>Tous les clients avec une carte de fidélité :</h2>
+    <ul>
+        <?php
+        foreach ($clientsWithFidelityCard as $client) {
+            echo '<li>' . $client["firstName"] . ' ' . $client["lastName"] . '</li>';
+        }
+        ?>
+    </ul>
+
 </body>
+
 </html>
