82f3b6b97b39badbf5aeac8ba8131e8a35552414
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2023-12-14 09:23:43 +0100

    exercice 1 correction

---
+++ exercice-1/index.php
@@ -0,0 +1,29 @@
+<?php
+require_once('./database_connect.php');
+
+$request = $database->query('SELECT * FROM clients');
+
+$clients = $request->fetchAll();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h2>Tous les clients :</h2>
+    <ul>
+    <?php
+       foreach($clients as $client){
+            echo '<li>' . $client["firstName"] . ' ' . $client["lastName"] . '</li>';
+       } 
+    ?>
+    </ul>
+    
+</body>
+</html>
